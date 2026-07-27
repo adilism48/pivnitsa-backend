25480a4c66aff4af08446bf404b6bbd252a85d8f
Adil <adilism48@gmail.com>
2026-07-27 11:39:14 +0600

    intial commit

---
+++ src/main/java/kg/megalab/pivnitsabackend/PivnitsaBackendApplication.java
@@ -0,0 +1,13 @@
+package kg.megalab.pivnitsabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivnitsaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PivnitsaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megalab/pivnitsabackend/PivnitsaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megalab.pivnitsabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivnitsaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
