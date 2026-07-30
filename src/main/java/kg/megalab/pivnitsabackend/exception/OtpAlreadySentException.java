7681063b5c92df9fa1694d5284383cedf0d30682
jukeshov erlan <jukeshov.erlan@gmail.com>
2026-07-30 14:17:15 +0600

    feat(otp): carry retryAfterSeconds in OtpAlreadySentException for countdown UI

--- src/main/java/kg/megalab/pivnitsabackend/exception/OtpAlreadySentException.java
+++ src/main/java/kg/megalab/pivnitsabackend/exception/OtpAlreadySentException.java
@@ -1,7 +1,15 @@
 package kg.megalab.pivnitsabackend.exception;
 
 public class OtpAlreadySentException extends RuntimeException {
-    public OtpAlreadySentException(String message) {
+
+    private final long retryAfterSeconds;
+
+    public OtpAlreadySentException(String message, long retryAfterSeconds) {
         super(message);
+        this.retryAfterSeconds = retryAfterSeconds;
+    }
+
+    public long getRetryAfterSeconds() {
+        return retryAfterSeconds;
     }
 }
