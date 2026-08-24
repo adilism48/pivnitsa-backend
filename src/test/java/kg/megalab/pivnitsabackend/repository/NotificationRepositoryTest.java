f4645e67a27d06221c0395bf0a55772962459dea
Adil <adilism48@gmail.com>
2026-08-25 01:39:33 +0600

    refactor(tests): singleton psql, redis containers for tests

--- src/test/java/kg/megalab/pivnitsabackend/repository/NotificationRepositoryTest.java
+++ src/test/java/kg/megalab/pivnitsabackend/repository/NotificationRepositoryTest.java
@@ -1,31 +1,18 @@
 package kg.megalab.pivnitsabackend.repository;
 
+import kg.megalab.pivnitsabackend.BaseIntegrationTest;
 import kg.megalab.pivnitsabackend.entity.Notification;
 import kg.megalab.pivnitsabackend.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@Testcontainers
-@AutoConfigureTestDatabase(
-        replace = AutoConfigureTestDatabase.Replace.NONE
-)
-class NotificationRepositoryTest {
-
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer postgres =
-            new PostgreSQLContainer("postgres:17-alpine");
+@Transactional
+class NotificationRepositoryTest extends BaseIntegrationTest {
 
     @Autowired private NotificationRepository notificationRepository;
     @Autowired private UserRepository userRepository;
--- src/test/java/kg/megalab/pivnitsabackend/repository/UserRepositoryTest.java
+++ src/test/java/kg/megalab/pivnitsabackend/repository/UserRepositoryTest.java
@@ -1,33 +1,15 @@
 package kg.megalab.pivnitsabackend.repository;
 
+import kg.megalab.pivnitsabackend.BaseIntegrationTest;
 import kg.megalab.pivnitsabackend.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-
-
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.postgresql.PostgreSQLContainer;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@Testcontainers
-@AutoConfigureTestDatabase(
-        replace = AutoConfigureTestDatabase.Replace.NONE
-)
-class UserRepositoryTest {
-
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer postgres =
-            new PostgreSQLContainer
-                    ("postgres:17-alpine");
+class UserRepositoryTest extends BaseIntegrationTest {
 
     @Autowired
     private UserRepository userRepository;
